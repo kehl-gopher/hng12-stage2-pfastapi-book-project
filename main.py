d99053835beff013f63cc0f7c1af807de8267b8e
kehl-gopher <kelanidarasimi9@gmail.com>
2025-02-13 22:39:17 +0100

    create a test webhook end point

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from api.router import api_router
 from api.routes.integration_conf import router as integration_router
+from api.routes.webhook import router as webhook_router
 from core.config import settings
 
 app = FastAPI()
@@ -17,6 +18,7 @@ app.add_middleware(
 
 # integration router
 app.include_router(integration_router)
+app.include_router(webhook_router)
 
 # api router
 app.include_router(api_router, prefix=settings.API_PREFIX)
